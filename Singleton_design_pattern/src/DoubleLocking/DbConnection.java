3eb88dfd77b813d431f2b589058283f2217cd319
snehithRaj1729 <snehithraj1729@gmail.com>
2024-12-25 18:37:51 +0530

    Abstract design pattern

--- Singleton_design_pattern/src/DoubleLocking/DbConnection.java
+++ Singleton_design_pattern/src/DoubleLocking/DbConnection.java
@@ -1,4 +1,17 @@
 package DoubleLocking;
 
 public class DbConnection {
+    private static DbConnection dbConnection;
+    private DbConnection(){}
+
+    public static DbConnection getInstance(){
+        if (dbConnection==null){
+            synchronized (DbConnection.class){
+                if(dbConnection==null){
+                    dbConnection=new DbConnection();
+                }
+            }
+        }
+        return dbConnection;
+    }
 }
